e1c50b9645970af32808eca2a86ad88b9d94d594
Whizark <contact@whizark.com>
2014-01-05 01:19:38 +0900

    add application Application

---
+++ src/App/Application.php
@@ -0,0 +1,14 @@
+<?php
+namespace App;
+
+use Silex\Application as BaseApplication;
+
+/**
+ * Class Application
+ *
+ * @package App\Application
+ * @author  Whizark
+ */
+class Application extends BaseApplication
+{
+}
