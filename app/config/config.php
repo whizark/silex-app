997ac10594b7c910e0d48f92efbc3c836380a0d9
Whizark <contact@whizark.com>
2014-01-06 09:02:10 +0900

    Config: fix comment for including bundle config(s)

--- app/config/config.php
+++ app/config/config.php
@@ -3,6 +3,6 @@ return function () use ($app) {
     call_user_func(require_once __DIR__ . '/parameters.php');
     call_user_func(require_once __DIR__ . '/services.php');
 
-    // Bundle configurations
+    // Bundle configuration(s)
     call_user_func(require_once __DIR__ . '/../../src/App/DefaultBundle/Resources/config/config.php');
 };
