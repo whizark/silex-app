0266a4c06dd533a0460858a577e90b8a67fb85c4
Whizark <contact@whizark.com>
2014-01-06 22:33:45 +0900

    Service: add Silex Web Profiler

--- app/config/services_dev.php
+++ app/config/services_dev.php
@@ -1,8 +1,12 @@
 <?php
+use Silex\Provider\WebProfilerServiceProvider;
 use Igorw\Silex\ConfigServiceProvider;
 
 return function () use ($app) {
     call_user_func(require_once __DIR__ . '/services.php');
+
+    // Silex Web Profiler
+    $app->register(new WebProfilerServiceProvider());
 
     // Config
     $app->register(
