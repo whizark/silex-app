7a1021e0e608b634873e2ff2f6dadb68761f4253
Whizark <contact@whizark.com>
2014-01-29 08:31:32 +0900

    add error handler

--- app/config/config.php
+++ app/config/config.php
@@ -1,6 +1,30 @@
 <?php
+use Symfony\Component\HttpFoundation\Response;
+
 return function () use ($app) {
     $app['app.parameters'] = [
         'app.root_dir' => __DIR__ . '/../..',
     ];
+
+    $app->error(
+        function (Exception $e, $code) use ($app) {
+            if ($app['debug'] || (isset($app['test']) && $app['test'])) {
+                return;
+            }
+
+            switch ($code) {
+                case 404:
+                    $message = 'The requested page could not be found.';
+
+                    break;
+
+                default:
+                    $message = 'We are sorry, but something went terribly wrong.';
+
+                    break;
+            }
+
+            return new Response($message);
+        }
+    );
 };
